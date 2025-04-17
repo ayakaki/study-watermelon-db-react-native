c2500667fb1421d8476fb85391f38e10bc519ff6
kakizawaayato <chibairodori25@gmail.com>
2025-04-18 07:07:25 +0900

    temporary

--- studyWatermelonDBApp/index.ts
+++ studyWatermelonDBApp/index.ts
@@ -1,6 +1,22 @@
 import { registerRootComponent } from 'expo';
 
+import { Database } from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
 import App from './App';
+import Post from './model/Post';
+import { mySchema } from './model/schema';
+
+const adapter = new SQLiteAdapter({
+  schema: mySchema,
+  onSetUpError: error => {
+    console.error('WatermelonDB setup error:', error);
+  }
+})
+
+const database = new Database({
+  adapter,
+  modelClasses: [Post],
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
