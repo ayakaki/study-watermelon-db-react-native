a4ad2aac4239f00725bc5e585441a57512c10091
kakizawaayato <chibairodori25@gmail.com>
2025-06-27 08:32:40 +0900

    🚧in progress: 暫定認証完了文章の実装

--- Cloud/supabase/functions/email-confirmed/index.ts
+++ Cloud/supabase/functions/email-confirmed/index.ts
@@ -1,14 +1,10 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
+import { serve } from 'https://deno.land/std/http/server.ts';
 
 serve((req: Request) => {
-  const url = new URL(req.url)
-  const isError = url.pathname.includes('error')
 
-  const message = isError
-    ? 'メール認証リンクが無効か期限切れです。再度認証メールを送信してください。'
-    : 'メール認証が完了しました。引き続きアプリをご利用ください。'
+  const msg = 'ご登録ありがとうございした。本アプリケーションをお楽しみください。';
 
-  return new Response(message, {
+  return new Response(msg, {
     headers: {
       'Content-Type': 'text/plain; charset=UTF-8'
     }
